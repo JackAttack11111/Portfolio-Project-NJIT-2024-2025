9de120872a93745a67dc9ef9fd0408126aff257e
JackAttack11111 <225jkonar@frhsd.com>
2024-10-11 10:40:44 -0400

    added working links to the contact page | Added text form for contact | Changed more icon to the contact icon for mobile | Optimized descriptions to be mobile friendly |
    20241011 1040am

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function mobilemenu() {
-    
+function contact() {
+    window.open("contact.html", "_self")
 }
+
+function home() {
+    window.open("index.html", "_self");
+}
+
+document.getElementsByClassName("logo").onclick = function() {home()};
